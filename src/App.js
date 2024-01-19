d723d018e518ba42d1cdb5b4c93299369db93cd4
guminseok <gumajuc@gmail.com>
2024-01-19 11:23:19 +0900

    mapを使ってAPPページ修正

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import Todo from "./components/Todo";
 
 function App(props) {
+
+  const taskList = props.tasks?.map((task) => (
+    <Todo id={task.id} name={task.name} completed={task.completed}/>
+  ));
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -45,9 +50,7 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <Todo />
-        <Todo />
-        <Todo />
+        {taskList}
       </ul>
     </div>
   );
